ef0a8e01a7d1c15527df68ccf10595bc720ac2fc
Matthew Tujague <matthewtujague@gmail.com>
2025-02-22 22:01:39 -0500

    create layout

--- src/app.py
+++ src/app.py
@@ -1,57 +1,79 @@
 #packages
-from flask import Flask, session, redirect, url_for, request, render_template
-from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from flask import Flask, redirect, url_for, request, jsonify
+from flask_login import UserMixin, LoginManager, login_user, logout_user, current_user
 from flask_socketio import SocketIO
+from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 
 #built-ins
 from dotenv import load_dotenv
-import time, threading
+import time, threading, json
 import os
 
 #local files
-from db import add_user, get_user
+# from db import add_user, get_user
+quantum_random_forest = lambda data: None 
 
 #app definition
 app = Flask(__name__)
+CORS(app)
 app.secret_key = os.environ.get("SECRET_KEY", "")
 socketio = SocketIO(app, cors_allowed_origins="*")
 login_manager = LoginManager(app)
 
+def aggregate_and_format_for_qrf(data: dict):
+    """
+    TODO define integration that aggregates previous user variables (if exists) in db
+    to those defined in data, grab activities data from activities bucket.
+
+    Compile into format for quantum random forest
+    """
+
+    return None
+
 #classes and helpers
 class User(UserMixin):
 
-    def __init__(self, unm: str, pw: str, ip: str, active=True):
-        self.id = unm + "@" + ip
-        
-        self.ip = ip
-        self.name = unm
-        self.pw = pw
-        
-        self.active = active #should be true if __init__ called from login scope, maybe not for copying from DBMS
+    def __init__(self, data: dict):
+        """
+        data:
+        {
+            "ip" : client_ip,
+            "userAgent" : ip of website,
+            "username" : username,
+            "password" : hashed (client side & salt & peppered) password
+            ...
+        }
+        """
 
-        self.activity_loop = threading.Thread(target=lambda: self.check_active)
+        self.id = data["ip"] + "@" + data["userAgent"]
+        self.username = data.get("username", None)
+        self.password = data.get("password", None)
+        
+        self.data = data
+        
+        self.active = True #should be true if __init__ called from login scope, maybe not for copying from DBMS
 
     def is_active(self): 
-        return self.active
-    
-    def check_active(self):
-        while True:
-            if self.activity_timeout_t > time.time():
-                self.active = False
-                #TODO store IP stats to db
-                break
-            time.sleep(0.01)
-    
+        return True
+        
     def get_id(self):
         return self.id
 
     @login_manager.user_loader
     def load_user(id):
-        # 1. Fetch against the database a user by `id` 
-        # 2. Create a new object of `User` class and return it.
-        u = get_user(id) #id is concat("username" + "@" + ip)
-        return User(u.un, u.pw, u.ip, u.active)
+        # u = get_user(id) #id is concat("username" + "@" + ip) TODO
+        u = {
+            "ip": "192.168.1.1",
+            "domain": "example.com",
+            "username": "sample_user",
+            "email": "sample_user@example.com",
+            "last_login": time.time(),
+            "activities": []  # This can be populated with activity data if needed
+        }
+
+        u["id"] = f"{u['ip']}@{u['domain']}"
+        return User(u.data)
 
     def is_anonymous(self): #no users will be conisdered guests on site, all must login
         return False
@@ -60,42 +82,97 @@ class User(UserMixin):
         return True
     
 #app implementation
-
 load_dotenv()
 
-
 @app.route('/')
-def index():
-    return render_template('index.html')  # Render the index.html file
+def visualization():
+    return redirect("https://walmart.com") #TODO change to streamlit location
 
-@socketio.on("update-activity") 
-def update_activity():
-    #will send periodic messages to this handler on different clients to remind DBMS and ML model that we should continue to consider
-    current_user.activity_timeout_t = time.time() + 5*60 # 5-min timeout
+@app.route("/api/validate_user")
+def _1():
+    form = lambda x: jsonify({"flag" : x})
+    body = request.data
 
-@app.route('/api/login/', methods=['POST'])
-def login():
-    """
-    Form elements will be replaced in future with API headers,
-    password will become API-key
-    """
-    domain = "http://localhost" #WILL BE SENT OVER AS RESPONSE HEADER TODO API
+    # u = get_user(body["username"])
 
-    unm = request.form['username']
-    passhash = generate_password_hash(os.environ.get("PEPPER", "") + request.form['password'])
-    ip = request.remote_addr
-    user_agent = request.headers.get('User-Agent')
+    # if u is not None:
+        # if(check_password_hash(u["hashed_password"], body['password'])):
+            # return form(1)
+        # return form(0)
+    # else:
+        # return form(-1)
 
-    login_user(User(unm, passhash, ip, user_agent, domain, active=True), remember=True) #will initialize activity timer & timeout
+    form(1)
 
-    return redirect(url_for('index'))
+@app.route("/api/send_preliminary_data", methods=['POST']) #DATA AQUISITION DONE
+def _2():
+    body = request.data
+    print("prelinimnary_data: ",body)
 
-@app.route('/api/logout', methods=["POST"])
-@login_required #TODO begone in case of API deployement
-def logout():
-    logout_user() #TODO manually stop timer and timeout
-    return redirect(url_for('index'))
+    #TODO grab variables, store
+
+    return jsonify({"status" : 200})
+
+@app.route('/api/login', methods=['POST'])
+def _3():
+    body = request.data
+    # print("login data: ",body)
+    try:
+        data = json.loads(body.decode("utf-8"))
+    except Exception as e:
+        print(f"ERR, cannot cast request body to JSON entity: {e}")
+
+    # Check if the user exists in the database
+    # u = get_user(body["username"])  # Uncomment and implement this line to fetch user from DB
+    # u = None  # Placeholder for user fetching logic
+
+    # if u is None:
+    #     return jsonify({"status": 404, "message": "User not found"}), 404
+
+    # # Verify the password
+    # if not check_password_hash(u["hashed_password"], request.form['password']):
+    #     return jsonify({"status": 401, "message": "Invalid password"}), 401
+
+    # login_user(u, remember=True)
+
+    print(data)
+
+    return jsonify({
+        "status" : 200,
+        "is-attacking": quantum_random_forest(aggregate_and_format_for_qrf(current_user))
+    })
+
+@app.route('/api/signup', methods=['POST'])
+def _4():
+    body = request.data
+
+    try:
+        data = json.loads(body.decode("utf-8"))
+    except Exception as e:
+        print(f"ERR, cannot cast request body to JSON entity: {e}")
+    
+    print(data)
+
+    # u = get_user(body["username"])  # Uncomment and implement this line to fetch user from DB
+
+    # if u is not None:
+    #     return jsonify({"status": 401, "message": f"User {data['username']} exists already"}), 404
+
+    # # Verify the password
+    # if not check_password_hash(u["hashed_password"], ['password']):
+    #     return jsonify({"status": 401, "message": "Invalid password"}), 401
+
+    # user = User(body) #make user
+
+    # login_user(user, remember=True)
+
+    # set_user(body)
+
+    return jsonify({
+        "status" : 200,
+        "is-attacking": quantum_random_forest(aggregate_and_format_for_qrf(current_user))
+    })
 
 if __name__ == '__main__':
     login_manager.init_app(app)
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)
--- src/db.py
+++ src/db.py
@@ -1,22 +1,12 @@
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
-from app import User
+
 import os
 
-bucket = "users"
 org = "Q-Safe"
 token = os.environ.get("INFLUX_TOKEN", "")
 
 url="http://localhost:8086"
-
-"""
-User Bucket:
-id[INDEX]: composite(ip,"@",domain) 
-domain: string,
-ip: string,
-unm: U(nullable, string),
-hpw: U(nullable, string),
-"""
 
 class DB:    
     def __init__(self):
@@ -28,35 +18,23 @@ class DB:
 
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
     
-    def add_user(self, ip: str, domain: str, username: str, password: str):
-        id_ = ip + "@" + domain
-        p = influxdb_client.Point(id_).tag("IP", ip).field("userdata", {
-            "ID" : id_,
-            "IP" : ip,
-            "DOMAIN" : domain,
-            "USERNAME" : username if username else "",
-            "PASSWORD" : password if password else "",
-        })
+    def add_user(self, data: dict):
+        p = influxdb_client.Point(data["id"]).field("userdata", data)
 
         self.write_api.write(bucket="users", org=org, record=p)
         
         return True
 
-    def add_activity(self, user_id, time_active, session_login_attempts, session_login_failures):
-        query = f'from(bucket: "activities") |> count()'
-        result = self.client.query_api().query(query, org=org)
-        document_count = sum([len(table.records) for table in result])
+    def add_activity(self, data: dict):
+        """
+        activities will have similar and often time identical userID's (uid)
+        any activity will have it's own unique id attr in the data dict however
 
-        p = influxdb_client.Point(document_count + 1).tag("UID", user_id).field("userdata", {
-            "ID" : user_id,
-            "TIME_ACTIVE" : time_active,
-            "SESSION_LOGIN_ATTEMPTS" : session_login_attempts,
-            "SESSION_LOGIN_FAILURES" : session_login_failures
-        })
+        indexing by uid will help to make searching faster for IP's on certain servers
+        """
+
+        p = influxdb_client.Point(data["id"]).tag("UID", data["uid"]).field("activitydata", data)
 
         self.write_api.write(bucket="activites", org=org, record=p)
         
         return True
-
-    def get_user(self, username):
-        return User.query.filter_by(username=username).first()
