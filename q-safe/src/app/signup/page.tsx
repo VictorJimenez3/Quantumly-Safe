7c55de6078f2d509e9819734c96847e85b48ec43
Ashan Deen <ash474d@gmail.com>
2025-02-23 01:48:51 -0500

    react and streamlit

--- q-safe/src/app/signup/page.tsx
+++ q-safe/src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { UAParser } from "ua-parser-js";
 import SHA256 from "crypto-js/sha256";
