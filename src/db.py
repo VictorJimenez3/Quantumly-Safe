a924471e7cbbbc66633d563e0e39a72c6e5ebeab
VictorJimenez3 <154997712+VictorJimenez3@users.noreply.github.com>
2025-02-22 22:01:40 -0500

    Database fixes and formatting, Data cleaning

--- src/db.py
+++ src/db.py
@@ -1,22 +1,10 @@
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
-from app import User
+from influxdb_client import InfluxDBClient
 import os
-
-bucket = "users"
-org = "Q-Safe"
-token = os.environ.get("INFLUX_TOKEN", "")
-
-url="http://localhost:8086"
-
-"""
-User Bucket:
-id[INDEX]: composite(ip,"@",domain) 
-domain: string,
-ip: string,
-unm: U(nullable, string),
-hpw: U(nullable, string),
-"""
+from dotenv import load_dotenv
+import json
+import uuid
 
 class DB:    
     def __init__(self):
@@ -27,36 +15,109 @@ class DB:
         )
 
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
-    
-    def add_user(self, ip: str, domain: str, username: str, password: str):
-        id_ = ip + "@" + domain
-        p = influxdb_client.Point(id_).tag("IP", ip).field("userdata", {
-            "ID" : id_,
-            "IP" : ip,
-            "DOMAIN" : domain,
-            "USERNAME" : username if username else "",
-            "PASSWORD" : password if password else "",
-        })
-
-        self.write_api.write(bucket="users", org=org, record=p)
+    #maybe serialize json so strings and boolean dont mess up the DB, or just have clean data
+    def add_interactions(self, data: dict):
+        #measurement name for each column, add later when data is finalized
         
-        return True
+        id_ = str(uuid.uuid4())
+        data = json.dumps(data)
+        #print(data, type(data))
+        p = influxdb_client.Point(id_).field("interactionData", data)
 
-    def add_activity(self, user_id, time_active, session_login_attempts, session_login_failures):
-        query = f'from(bucket: "activities") |> count()'
-        result = self.client.query_api().query(query, org=org)
-        document_count = sum([len(table.records) for table in result])
-
-        p = influxdb_client.Point(document_count + 1).tag("UID", user_id).field("userdata", {
-            "ID" : user_id,
-            "TIME_ACTIVE" : time_active,
-            "SESSION_LOGIN_ATTEMPTS" : session_login_attempts,
-            "SESSION_LOGIN_FAILURES" : session_login_failures
+        self.write_api.write(bucket="Interactions", org=org, record=p)
+        
+    
+    def add_user_record(self, username:str, password:str, ip:str, domain:str, user_agent:str):
+        self.add_interactions({
+            "total_log_in_count":0,
+            "failed_log_in_count": 0,
+            "ip":ip,
+            "domain":domain,
+            "username":username,
+            "password":password,
+            "user_agent":user_agent
         })
+    
+    def aggregate_user_signin(ip:str, domain:str, username:str, failed_log_in_count:int, total_log_in_count:int):
+        #Find the input with ip, domain, & username equal to parameters
+        #
+        #Add failed and total login count to existing record
+        
+
+
+        for row in range(8): #moving through dictionary? lowk confused myself with the local non local shi ask
+            if row["ip"] == ip and row["domain"] == domain and row[username] == "username":
+                row["failed_log_in_count"] += 1
+                row["total_log_in_count"] += 1
+
+        
+    
+
+    '''
+    def add_activity(self, data: dict):
+        """
+        activities will have similar and often time identical interactionID's (uid)
+        any activity will have it's own unique id attr in the data dict however
+
+        indexing by uid will help to make searching faster for IP's on certain servers
+        """
+        id_ = data["id"]
+
+        data = json.dumps(data)
+        p = influxdb_client.Point(id_).field("interactiondata", data)
+
 
         self.write_api.write(bucket="activites", org=org, record=p)
         
         return True
+        
+    '''
 
-    def get_user(self, username):
-        return User.query.filter_by(username=username).first()
+org = "Q-Safe"
+
+load_dotenv()
+
+# Environment and connection settings
+token = os.getenv("INFLUXDB_TOKEN")
+
+url="http://localhost:8086"
+
+bucket1 = "Interactions"
+db = DB()
+
+client = InfluxDBClient(url=url, token=token, org=org)
+
+def initilize_bucket(bucket:str):
+    #this is for testing, it makes the current data dissapear for the new data, remove in final version
+
+    buckets_api = client.buckets_api()
+    try:
+        bucket_obj = buckets_api.find_bucket_by_name(bucket)
+        bucket_id = bucket_obj.id
+        buckets_api.delete_bucket(bucket_id)
+        print(f"Deleted existing bucket: {bucket}")
+    except Exception as e:
+        print(f"Bucket {bucket} not found or already deleted. Error: {e}")
+        
+    retval = buckets_api.create_bucket(bucket_name=bucket, org=org)
+    print(f"Created new bucket: {bucket} {retval}")
+
+
+
+initilize_bucket(bucket1)
+
+person = {
+    #id is randomly generated
+    "total_log_in_count":"6",
+    "failed_log_in_count": "23",
+    "ip":"1.255.453",
+    "domain":"apple.com",
+    "username":"Stebe Jobs",
+    "password":"password",
+    "user_agent":"firefox"
+}
+
+
+db.add_interactions(person)
+
+
